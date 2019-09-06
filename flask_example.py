0ecec9c34350cf23b91737d81a81394d53b2e0f1
Shane Urban <surbanbusiness@gmail.com>
2019-09-05 21:45:37 -0600

    Flask Kieran test
    test code created by kieran

--- flask_example.py
+++ flask_example.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
-def index():
-    return "Hello, World!"
-
+def view_list():
+    shopping_list = ["eggs", "ham", "Sausage"]
+    printed_list = "<ul>" # start with an open unordered list
+    for item in shopping_list:
+      printed_list += f"<li>{item}</li>" # add the current item as a list item
+    printed_list += "</ul>" # end the unordered list
+    return printed_list
+        
 if __name__ == "__main__":
     app.run(debug=True)
