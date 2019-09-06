fb79c291c6828d35d2973ee4d53be68c07dd0057
Shane Urban <surbanbusiness@gmail.com>
2019-09-06 12:44:53 -0600

    Shopping List Testing Ground

--- Flask_Shopping_list_test.py
+++ Flask_Shopping_list_test.py
@@ -12,16 +12,16 @@ def view_list():
     * Figure out how to make an HTML form to add a new item (let me know if you need guidence)
   """
     # an HTML representation of the user shopping list
-printed_list = html_list(user["shopping_list"]) 
+    printed_list = html_list(user["shopping_list"])
 
-return printed_list
-​
+    return printed_list
+
 @app.route("/hello")
 def hello():
   """ When someone goes to /hello the browser
   will just return hello"""
   return "Hello"
-​
+
 def list_to_html(shopping_list):
   """Takes in a python list and returns the list
   as a string of HTML content.
@@ -43,3 +43,4 @@ if __name__ == "__main__":
       "age":10,
       "shopping_list": ["eggs", "ham", "sausages"]
     }
+
